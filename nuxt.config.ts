9140b533a213b7b8abc18c2d5851e57f63d4c702
Erick <erick.dev.miller@gmail.com>
2025-02-28 13:40:57 +0500

    build fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
       preprocessorOptions: {
         scss: {
           additionalData: `
-            @import "@/assets/styles/_global.scss";
+            @use "@/assets/styles/_global.scss" as *;
           `,
         },
       },
